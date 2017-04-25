76264d02cd8ee8923a5761b801772577e375e510
Ekaphong Tangtrakul <t.ekaphong@gmail.com>
2017-04-25 13:43:32 +0700

    LAB05

---
+++ src/main/java/rmuti/lab05/ArrayListStack.java
@@ -0,0 +1,28 @@
+package rmuti.lab05;
+
+import java.util.ArrayList;
+
+public class ArrayListStack {
+
+	private ArrayList arr = new ArrayList();
+
+	public void push(Object e) {
+		arr.add(e);
+	}
+
+	public void pop(int value) {
+		int n = (int) arr.size() - 1;
+		for (int i = n; i >= value; i--) {
+			arr.remove(i);
+		}
+
+	}
+	public Object peek() {
+        return arr.get(arr.size()-1);
+    }
+
+	public Object getData() {
+		return arr;
+	}
+
+}
